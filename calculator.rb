90d12025378a0b4ee1908a2bd942f1785603e2a1
Adrian Samaniego <llathander@gmail.com>
2020-01-10 21:08:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,2 @@
 
-first_number = 1
-second_number = 56
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
-
 
