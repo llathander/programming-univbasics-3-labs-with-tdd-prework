34ae7a6776a27609234cd5355818895d2dc567de
Adrian Samaniego <llathander@gmail.com>
2020-01-10 21:12:39 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,8 @@
 
+
 first_number = 1
-second_number = 56
+second_number = 5
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
-
-
